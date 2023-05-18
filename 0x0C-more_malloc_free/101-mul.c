75c86c42d1ece0f085d789f9eefd8a21d1cdf47f
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-05-18 16:22:23 -0700

    task adv

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,11 +17,11 @@ void _print(char *str, int l)
 		if (str[i] != '0')
 			j = 1;
 		if (j || i == l - 1)
-			return (write(1, &str[i], 1));
+			_putchar(str[i]);
 		i++;
 	}
 
-	return (write(1, '\n', 1));
+	_putchar('\n');
 	free(str);
 }
 
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
-			return (write(1, &e[ti], 1));
+			_putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
@@ -128,7 +128,7 @@ int main(int argc, char *argv[])
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
-			return (write(1, &e[ti], 1));
+			_putchar(e[ti]);
 		exit(98);
 	}
 	init(a, ln - 1);
@@ -138,7 +138,7 @@ int main(int argc, char *argv[])
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
-				return (write(1, &e[ti], 1));
+				_putchar(e[ti]);
 			free(a);
 			exit(98);
 		}
