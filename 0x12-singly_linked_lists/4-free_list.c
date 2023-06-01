023be2b43951b65b1dc91c31995bf18dcd974269
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-06-01 16:58:21 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list.
+ * @head: A pointer.
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
