b9596a02f79a6eade6e2578681d0d2e5e7aab0a5
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-06-01 17:54:02 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,11 +15,37 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
+/**
+ * print_list - task 0
+ * @h: xx
+ * Return: xx
+ */
 size_t print_list(const list_t *h); /*t0*/
+/**
+ * list_len - task 1
+ * @h: xx
+ * Return: xx
+ */
 size_t list_len(const list_t *h); /*t1*/
+/**
+ * add_node - task 2
+ * @head: kk
+ * @str:hhh
+ * Return: nn
+ */
 list_t *add_node(list_t **head, const char *str);/*t2*/
+/**
+ * add_node_end - task 3
+ * @head: jjj
+ * @str: hh
+ * Return: dd
+ */
 list_t *add_node_end(list_t **head, const char *str);/*t3*/
+/**
+ * free_list - task 4
+ * @head: xx
+ * Return: xx
+ */
 void free_list(list_t *head);/*t4*/
 
 #endif
