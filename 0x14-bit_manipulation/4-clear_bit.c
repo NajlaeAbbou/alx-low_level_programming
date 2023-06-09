6fcea253b6b0e9b62f51fb1d707f5c4110c47d25
NajlaeHadjAbbou <hadjabbou.najlae@gmail.com>
2023-06-09 02:35:11 +0100

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - xxx
+ * @n: xxx
+ * @index: xxx
+ * Return: xxx
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = ~(1 << index);
+	*n = *n & i;
+
+	return (1);
+}
