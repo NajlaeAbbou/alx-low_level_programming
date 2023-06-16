06e3e0bc306dba401e0287e219ae111e9b585cff
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-06-16 16:11:52 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buff;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
@@ -19,8 +19,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
