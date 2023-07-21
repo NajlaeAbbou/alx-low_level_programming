1db25d6f17fb51e4572170c0d97e42f0b6c3af6b
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-20 19:45:21 -0700

    103

---
+++ 0x17-doubly_linked_lists/old.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	char key[7] = "      ";
+	size_t len = strlen(argv[1]);
+	char max_char = argv[1][0];
+	int random_seed = argv[1][0];
+	size_t i;
+
+	if (argc != 2)
+	{
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
+	}
+
+	key[0] = 'A' + (len ^ 59) % 26;
+	key[1] = 'A' + (len ^ 79) % 26;
+	key[2] = 'A' + (len ^ 85) % 26;
+	for (i = 1; i < len; i++)
+	{
+		if (argv[1][i] > max_char)
+		{
+			max_char = argv[1][i];
+		}
+	}
+	key[3] = max_char;
+	key[4] = 'A' + ((len * len) ^ 239) % 26;
+
+	for (i = 1; i < len; i++)
+	{
+		random_seed ^= argv[1][i];
+	}
+	srand(random_seed);
+	key[5] = 'A' + rand() % 26;
+	printf("%s\n", key);
+	return (0);
+}
+
