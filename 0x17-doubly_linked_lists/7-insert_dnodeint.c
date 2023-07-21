232145876a340248d907d2b0ef88e5a17d1bcaf8
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-20 18:11:54 -0700

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *head;
 	unsigned int i = 1;
-	
+
 	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
