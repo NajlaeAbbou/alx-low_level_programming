5d7289ea70a43d5905f70f9fb6b2c06a0c42e817
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-20 18:05:11 -0700

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	newh = *head;
+	new->next = newh;
 	*head = new;
 	if (newh != NULL)
-		(newh)->prev = new;
+		newh->prev = new;
 	return (new);
 }
