1b63c9eea2f929cff8298430c1dba818a87da4d1
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-20 18:58:14 -0700

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,31 +11,32 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int count = 0;
 
 	if (*head == NULL)
-		return -1;
+		return (-1);
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(current);
-		return 1;
+		return (1);
 	}
 	while (current != NULL)
 	{
 		if (count == index - 1)
 		{
 			dlistint_t *node_to_delete = current->next;
+
 			if (node_to_delete == NULL)
-				return -1;
+				return (-1);
 			current->next = node_to_delete->next;
 			if (node_to_delete->next != NULL)
 				node_to_delete->next->prev = current;
 			free(node_to_delete);
-			return 1;
+			return (1);
 		}
 		current = current->next;
 		count++;
 	}
-	return -1;
+	return (-1);
 }
 
