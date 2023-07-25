4755d9c2ef3e8efbc90eac1a91ca655c7f2ef875
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-24 19:25:01 -0700

    adv1

---
+++ 0x18-dynamic_libraries/functionop.c
@@ -0,0 +1,58 @@
+/**
+* add - adds two int
+* @a: int
+* @b: int
+* Return: sum
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two int
+* @a: int
+* @b: int
+* Return: subtract
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two int
+* @a: int
+* @b: int
+* Return: multiple
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds two int
+* @a: int
+* @b: int
+* Return: dividend
+*/
+int div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	return (0);
+}
+
+/**
+ * mod - finds the modulus of two int
+ * @a: int
+ * @b: int
+ * Return: modulus
+*/
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	return (0);
+}
