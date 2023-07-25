72d5f14dad64341da7860e15f7f9f41367ffedea
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-24 19:46:44 -0700

    adv2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NajlaeAbbou/alx-low_level_programming/raw/master/0x18-dynamic_libraries/random.so
-export LD_PRELOAD=/tmp/random.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
